047c342387fa78db3a88f4a04b8764d3fc015542
Abid Hasan <developer.abid@gmail.com>
2019-09-13 15:33:05 +0600

    micro service added

---
+++ GoGamesMicroService/games/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from games.serializers import GameSerializer
+from .models import Game
+
+
+class GamesApi(APIView):
+
+    def get(self, request):
+        games = Game.objects.all()
+        category = self.request.query_params.get('category', 0)
+        if category is 0:
+            serializer = GameSerializer(games, many=True)
+        else:
+            serializer = GameSerializer(games.filter(category=category), many=True)
+        return Response(serializer.data)
